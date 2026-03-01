5594c8f7556102e22077ec32aacaba472e869468
Nischal Pandey <162779921+Nischal-pandey@users.noreply.github.com>
2026-03-01 17:03:55 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,47 @@
+# age = 18
+# if age >= 18:
+#     print("Vote de sakta hai! 🗳️")
+# else:
+#     print("Abhi chhota hai, ruk ja! ")
+    
+    
+# num = 7
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# value = 5
+# if value >= 0:
+#     print("Positive")
+# else:
+#     print("Negative")
+
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are eligible to vote!")
+# else:
+#     print("You are not eligible to vote.")
+
+    
+# year = 2028
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(year, "is a leap year.")
+# else:
+#     print(year, "is not a leap year.")
+
+# a = 55
+# b = 66
+# if a > b :
+#     print("a is greater than b")
+# else:
+#     print("b is greater than a")
+
+
+char = input("Enter a character: ").lower()
+if char in "aieou":
+    print("{char} is a vowel")
+else:
+    print("{char} is a Consonant")
